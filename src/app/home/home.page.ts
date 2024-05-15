6d6c452935b72d939bc98e4082b2b738ffb36e85
Jabulani00 <jabulanimosesgwala@gmail.com>
2024-05-15 13:34:43 +0200

    gg

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private router: Router, private loadingController: LoadingController) {}
 
-  constructor() {}
+  async ngOnInit() {
+    setTimeout(() => {
+      this.router.navigate(['/login']); // Navigate to the login page after a delay
+    }, 3000);
+  }
 
 }
