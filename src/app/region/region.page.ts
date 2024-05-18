0eba788f0f671b3bf12e02ef0a95f7e95994838f
Jabulani00 <jabulanimosesgwala@gmail.com>
2024-05-18 16:31:07 +0200

    1235

--- src/app/region/region.page.ts
+++ src/app/region/region.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {   ToastController , AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-region',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegionPage implements OnInit {
 
-  constructor() { }
+  userDocument: any;
+  navController: NavController;
+
+  constructor(  private alertController: AlertController
+               ,private navCtrl: NavController,
+              private auth: AngularFireAuth,
+              private db: AngularFirestore,
+              private toastController: ToastController) {this.navController = navCtrl;}
 
   ngOnInit() {
+  }
+  async presentConfirmationAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirmation',
+      message: 'Are you sure you want to SIGN OUT?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+         cssClass: 'my-custom-alert',
+          handler: () => {
+            console.log('Confirmation canceled');
+          }
+        }, {
+          text: 'Confirm',
+          handler: () => {
+           
+            
+            this.auth.signOut().then(() => {
+              this.navController.navigateForward("/login");
+              this.presentToast()
+        
+        
+            }).catch((error) => {
+            
+            });
+  
+  
+  
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+  
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'SIGNED OUT!',
+      duration: 1500,
+      position: 'top',
+    
+    });
+  
+    await toast.present();
   }
 
 }
